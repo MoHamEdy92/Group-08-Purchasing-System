bdeb4d1acd008d24cd03c28f575d9891f3eaac3d
Chin Wei Teoh <chinwei2060@outlook.com>
2019-09-27 00:12:36 -0700

    Solving MultiValueDictKeyError

--- Purchasing System/RequestForQuotation/views.py
+++ Purchasing System/RequestForQuotation/views.py
@@ -253,13 +253,14 @@ def requestforquotationdetails(request):
 
 def requestforquotationhistorydetails(request):
 
-    print(request.body)
-    pk = request.GET['rfq_id']
-    request_for_quotation = RequestForQuotation.objects.get(request_for_quotation_id = pk)
-    items = RequestForQuotationItem.objects.filter(request_for_quotation_id = pk)
-
-    print(request_for_quotation.person_id)
-    context = {
+    try:
+        print(request.body)
+        pk = request.GET['rfq_id']
+        request_for_quotation = RequestForQuotation.objects.get(request_for_quotation_id = pk)
+        items = RequestForQuotationItem.objects.filter(request_for_quotation_id = pk)
+    except MultiValueDictKeyError:
+        print(request_for_quotation.person_id)
+        context = {
 
             'title': 'Request For Quotation Details',
             'purchase_requisition_id' : request_for_quotation.purchase_requisition_id.pr_id,
@@ -272,9 +273,9 @@ def requestforquotationhistorydetails(request):
             'grand_total': request_for_quotation.total_price,
             'time_created': request_for_quotation.time_created,
             'description' : request_for_quotation.description
-        }
+            }
   
-    return render(request,'RequestForQuotation/requestforquotationhistorydetails.html',context)
+        return render(request,'RequestForQuotation/requestforquotationhistorydetails.html',context)
 
 def requestforquotationhistory(request):
 
